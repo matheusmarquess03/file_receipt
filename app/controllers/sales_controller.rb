f741ee40fb8b8ec6f34a50b40cde2b562acd497d
Matheus Marques <matheus.marques@leadstation.com.br>
2022-09-02 14:35:57 -0300

    Sales view

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,12 @@
 class SalesController < ApplicationController
   def index
+    def index
+      @sales = Sale.all
+      @sales = @sales.paginate(:page => params[:page], :per_page => 10)
+    end
+
+    def import
+      raise 'File not found!' if params[:file].blank?
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "sales#index"
-  resources :sales, only: [:index]
+  resources :sales, only: [:index] do
+    collection do
+      post :import
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
